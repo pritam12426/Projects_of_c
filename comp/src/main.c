d19546aa824c52e9ec5a874495e56bf0032d4d70
Pritam <84720825+pritam12426@users.noreply.github.com>
2024-01-13 05:16:15 +0530

    FIX: extra tokens at end of #ifdef directive
    issue is with the #ifdef line.
    The #ifdef directive is typically followed by a single macro name
    to check if it is defined. However, in this case, you have
    used the logical OR (||) operator, which is causing the warning.

--- comp/src/main.c
+++ comp/src/main.c
@@ -5,11 +5,12 @@
 #include "./lib/user_function.h"
 
 
-#ifdef __APPLE__ || linux
+#if defined(__APPLE__) || defined(linux)
 	#define PYTHON "python3"
-#elif __WIN32
+#elif defined(__WIN32)
 	#define PYTHON "python"
 #endif
+
 
 
 int main(int argc, char *argv[]) {
@@ -25,6 +26,7 @@ int main(int argc, char *argv[]) {
 
 	if (strEndWith(argv[1], ".py")) {
 		char python_command[len_argv + 9];
+		printf("[\033[1;35mTRYING TO COMPILE\033[0m] { \033[1;36m%s\033[0m }\n", python_command);
 		sprintf(python_command, "%s %s", PYTHON, argv[1]);
 		return system(python_command);
 	}
