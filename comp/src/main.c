8ca61dc717acdb22c8e2e1c0edbfc129ce633d6f
Pritam <84720825+pritam12426@users.noreply.github.com>
2024-01-15 21:14:46 +0530

    Added: A function for executing C code in the side
    the temp directory

--- comp/src/main.c
+++ comp/src/main.c
@@ -21,7 +21,7 @@
 #endif
 
 
-static char *temp_dir;
+static char *TEMP_DIR;
 
 
 int main(int argc, char *argv[]) {
@@ -41,7 +41,7 @@ int main(int argc, char *argv[]) {
 		printf("[\033[1;35mRUNNING\033[0m] { \033[1;36m%s\033[0m }\n", python_command);
 		return system(python_command);
 	}
-	else if (str_ends_with(argv[1], ".js")) {
+	else if (strEndWith(argv[1], ".js")) {
 		char JScommand[len_argv + 5];
 		snprintf(JScommand, sizeof(JScommand), "%s %s", "node", argv[1]);
 		printf("[\033[1;35mRUNNING\033[0m] { \033[1;36m%s\033[0m }\n", JScommand);
@@ -55,6 +55,60 @@ int main(int argc, char *argv[]) {
 	}
 	else {
 		file_name++;
+	}
+
+	#ifdef linux
+		TEMP_DIR = getenv("TMPDIR");
+	#elif __APPLE__
+		TEMP_DIR = getenv("TMPDIR");
+	#elif __WIN32
+		TEMP_DIR = "C:\\WINDOWS\\Temp\\";
+	#else
+		printf("\033[1;31mUNSUPPORTED SYSTEM\033[0m:(\n");
+		return 1;
+	#endif
+
+	size_t len_file_name = strlen(file_name) + 1;
+	char actual_file_name[len_file_name];
+	strcpy(actual_file_name, file_name);
+	
+	chStrLower(actual_file_name);
+
+	chStrReplace(actual_file_name, ' ', '_');
+	chStrReplace(actual_file_name, '.', '_');
+
+	size_t len_temp_dir = strlen(TEMP_DIR);
+	char _command[len_argv + len_file_name + len_temp_dir + 18];
+
+
+	if(strEndWith(argv[1], ".c")){
+		sprintf(_command, "gcc %s %s %s%s%s", argv[1], "-o", TEMP_DIR, actual_file_name, FILE_EXTENSION);
+	}
+	else if(strEndWith(argv[1], ".cpp")){
+		sprintf(_command, "g++ %s %s %s%s%s", argv[1], "-o", TEMP_DIR, actual_file_name, FILE_EXTENSION);
+	}
+	else if(strEndWith(argv[1], ".rs")){
+		sprintf(_command, "rustc %s %s %s%s%s", argv[1], "-o", TEMP_DIR, actual_file_name, FILE_EXTENSION);
+	}
+	else if(strEndWith(argv[1], ".java")){
+		sprintf(_command, "javac %s %s %s%s%s", argv[1], "-o", TEMP_DIR, actual_file_name, FILE_EXTENSION);
+		// TODO: Write the logic for compiling the '.java' files :)..
+	}
+	else{
+		printf("\033[1;31mUNSUPPORTED FILE TYPE\033[0m: { \033[1;36m%s\033[0m }.\n", file_name);
+		return 2;
+	}
+	int return_value = -1;
+
+	printf("[\033[1;35mTRYING TO COMPILE\033[0m] { \033[1;36m%s\033[0m }\n", _command);
+	return_value = system(_command);
+
+	if (return_value == 0) {
+		char another_command[len_temp_dir + len_file_name];
+		sprintf(another_command, "%s%s%s", TEMP_DIR, actual_file_name, FILE_EXTENSION);
+		printf("[\033[1;35mRUNNING\033[0m] { \033[1;36m%s\033[0m }\n", another_command);
+
+		if (system(another_command) == 0) putchar('\n');
 	}
 
 	return 0;
