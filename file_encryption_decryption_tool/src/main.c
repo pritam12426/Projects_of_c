121f43982f2b131001aea5cad2428213fb05d124
Pritam <84720825+pritam12426@users.noreply.github.com>
2024-03-17 07:49:50 +0530

    Added a .git Ignore

--- file_encryption_decryption_tool/src/main.c
+++ file_encryption_decryption_tool/src/main.c
@@ -4,7 +4,10 @@
 
 #include "lib/user_function.h"
 
-int main(int argc, char *argv[]) {
-	;
+
+int main(int argc, const char *argv[]) {
+	for (size_t i = 1; i < argc; i++) {
+		printf("%s\n", argv[i]);
+	}
 	return 0;
 }
