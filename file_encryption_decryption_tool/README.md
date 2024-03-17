622d370b7595abe1ec05da857b6c214867f62c0e
Pritam <84720825+pritam12426@users.noreply.github.com>
2024-03-17 07:46:17 +0530

    Included a new project file.

---
+++ file_encryption_decryption_tool/README.md
@@ -0,0 +1,2 @@
+# File Encryption | Decryption Tool
+A programme which can encrypting for Decrypting and UTF-8 text files.
---
+++ file_encryption_decryption_tool/src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <string.h>
+
+#include "lib/user_function.h"
+
+int main(int argc, char *argv[]) {
+	;
+	return 0;
+}
